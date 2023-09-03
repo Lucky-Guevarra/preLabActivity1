8b8b58aa6ca2fc238740561d240326028c31a582
Lucky Angel Santos Guevarra <143337332+Lucky-Guevarra@users.noreply.github.com>
2023-09-03 16:28:17 +0800

    Update pre-lab-act-2-week-1.py

--- pre-lab-act-2-week-1.py
+++ pre-lab-act-2-week-1.py
@@ -1,3 +1,7 @@
+#If you run a 10-kilometer race in 43 minutes and 30 seconds,
+#what is your average time per mile?
+#What is your average speed in miles per hour?
+#(Hint: there are 1.61 kilometers in a mile).
 def textto (text):
     return "*" + text + "*"
 asterisk = "*" * 58
