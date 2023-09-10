4e238abb27a9cfdcca10a83e96d6208ebf608c52
Lucky Angel Santos Guevarra <143337332+Lucky-Guevarra@users.noreply.github.com>
2023-09-10 20:01:56 +0800

    Update pre-lab-act-2-week-1.py

--- pre-lab-act-2-week-1.py
+++ pre-lab-act-2-week-1.py
@@ -8,19 +8,13 @@ asterisk = "*" * 58
 
 km_to_m = 10 / 1.61
 
-avrg_km_ran = round(43.5 / 10, 2)
-avrg_kmph_ran = round(60 / avrg_km_ran, 2)
-avrg_m_ran = round(43.5 / km_to_m, 2)
-avrg_mph_ran = round(60 / avrg_m_ran, 2)
+a = round(43.5/km_to_m, 2)
+b = round(km_to_m/(43.5/60),2)
 
 print(asterisk)
 print()
-print(textto(("Average Kilometer Per Minute Speed: " + str(avrg_km_ran) + " km/m").center(56)))
+print(textto(("Average time per mile: "+str(a)+" min/mile").center(56)))
 print()
-print(textto(("Average Kilometer Per Hour Speed: " + str(avrg_kmph_ran) + " km/h").center(56)))
-print()
-print(textto(("Average Mile Per Minute Speed:  " + str(avrg_m_ran) + " km/m").center(56)))
-print()
-print(textto(("Average Mile Per Hour Speed: " + str(avrg_mph_ran) + " km/h").center(56)))
+print(textto(("Average Speed in miles per hour: "+str(b)+" m/h").center(56)))
 print()
 print(asterisk)
